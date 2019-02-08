55c378d457304741377791b65546125cbaec8dc9
kavinraj06 <kavinraj@angleritech.com>
2019-02-08 12:38:42 +0530

    loaded the data via json

--- src/app/static/static.component.ts
+++ src/app/static/static.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-static',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaticComponent implements OnInit {
 
-  constructor() { }
+  constructor (private httpService: HttpClient) { }
+  arrBirds: string [];
 
   ngOnInit() {
+    this.httpService.get('../../assets/beercraft.json').subscribe(
+      data => {
+        this.arrBirds = data as string [];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.arrBirds[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+
   }
 
 }
